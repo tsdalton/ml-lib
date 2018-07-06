73dd5add45e67f1b8dbd512381129a85fa13f28e
Thomas Dalton <tdalton@webroot.com>
2018-07-06 14:00:01 -0700

    add basic project structure

---
+++ python/ml_libr/linear/__init__.py
@@ -0,0 +1,7 @@
+"""
+The :mod:`ml_libr.linear_model` module implements generalized linear models.
+"""
+
+from .perceptron import Perceptron
+
+__all__ = ['Perceptron']
---
+++ python/ml_libr/linear/perceptron.py
@@ -0,0 +1,16 @@
+# Author: Thomas Dalton
+
+
+class Perceptron():
+    """Perceptron learning algorithm.
+
+    References
+    ----------
+
+    https://en.wikipedia.org/wiki/Perceptron
+
+    """
+
+    def __init__(self):
+        pass
+
